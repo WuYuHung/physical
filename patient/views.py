b80b4071cbab2b6575c87418ed3e60fd55b6c5c2
吳昱宏 <edward.0726@gmail.com>
2020-01-01 21:12:42 +0800

    feat: add patient

--- patient/views.py
+++ patient/views.py
@@ -1,76 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import plotly.figure_factory as ff
-
+from .models import Task
+from datetime import timedelta
 
 
 def index(request):
-    df = [
-        dict(
-            Task="華國瑜",
-            Start="2016-01-01 6:00:00",
-            Finish="2016-01-01 6:08:00",
-            Resource="Blood test",
-        ), 
-        dict(
-            Task="華國瑜",
-            Start="2016-01-01 6:10:00",
-            Finish="2016-01-01 6:20:00",
-            Resource="Vision test",
-        ),
-        dict(
-            Task="華國瑜",
-            Start="2016-01-01 6:22:00",
-            Finish="2016-01-01 6:30:00",
-            Resource="Hearing test",
-        ),
-        dict(
-            Task="華國瑜",
-            Start="2016-01-01 6:32:00",
-            Finish="2016-01-01 6:38:00",
-            Resource="X-ray test",
-        ),
-        dict(
-            Task="華國瑜",
-            Start="2016-01-01 6:40:00",
-            Finish="2016-01-01 6:50:00",
-            Resource="Urine routine",
-        ),
-        dict(
-            Task="俞國華",
-            Start="2016-01-01 6:08:00",
-            Finish="2016-01-01 6:16:00",
-            Resource="Blood test",
-        ), 
-        dict(
-            Task="俞國華",
-            Start="2016-01-01 6:20:00",
-            Finish="2016-01-01 6:30:00",
-            Resource="Vision test",
-        ),
-        dict(
-            Task="俞國華",
-            Start="2016-01-01 6:32:00",
-            Finish="2016-01-01 6:40:00",
-            Resource="Hearing test",
-        ),
-        dict(
-            Task="俞國華",
-            Start="2016-01-01 6:42:00",
-            Finish="2016-01-01 6:50:00",
-            Resource="X-ray test",
-        ),
-        dict(
-            Task="俞國華",
-            Start="2016-01-01 6:52:00",
-            Finish="2016-01-01 7:02:00",
-            Resource="Urine routine",
-        ),
-    ]
+    username = None
+    if request.user.is_authenticated:
+        username = request.user.username
+    tasks = Task.objects.filter(patient=username)
+    df = list()
+    for task in tasks:
+        temp_dict = dict(Task=task.kind)
+        temp_dict["Task"] = task.kind
+        dt = task.start_time
+        temp_dict["Start"] = dt.strftime("%Y-%m-%d %H:%M:%S")
+        temp_dict["Finish"] = (dt + timedelta(minutes=5)).strftime("%Y-%m-%d %H:%M:%S")
+        temp_dict["Resource"] = task.kind
+        df.append(temp_dict)
     fig = ff.create_gantt(
         df, index_col="Resource", show_colorbar=True, group_tasks=True
     )
     string = fig.to_html()
 
-    return render(request, "patient.html", {"chart": string})
+    return render(request, "patient.html", {"chart": string, "username": username})
 
