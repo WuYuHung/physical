fd34d664b1c05bddda1d85c1ff88a295915cfcda
吳昱宏 <edward.0726@gmail.com>
2020-01-02 12:47:13 +0800

    feat: seperate

--- doctor/views.py
+++ doctor/views.py
@@ -3,6 +3,9 @@ from django.views.generic import TemplateView
 from doctor.forms import HomeForm
 from datetime import datetime
 from django.contrib.auth.models import User, Permission
+import json
+from patient.models import Task
+from django.utils import timezone
 
 # def index(request):
 #     if request.user.is_authenticated and request.user.has_perm("doctor.doctor"):
@@ -25,9 +28,16 @@ class HomeView(TemplateView):
         form = HomeForm(request.POST)
         if form.is_valid():
             instance = form.save(commit=False)
-            instance.start_time = datetime.now()
-            instance.doctor = username
-            instance.save()  # Add
+            kinds = json.loads(instance.kind.replace("'", '"'))
+            print(datetime.now())
+            for kind in kinds:
+                instance.kind = kind
+                Task.objects.create(
+                    patient=instance.patient,
+                    kind=kind,
+                    doctor=username,
+                    start_time=timezone.now(),
+                )
             user = User.objects.create_user(
                 username=instance.patient, password=instance.patient
             )
