722ec1ab6ed23a6638fb7ba6df41a8fb989af70e
michael071020 <39074199+michael071020@users.noreply.github.com>
2020-01-02 18:32:09 +0800

    schedule

--- doctor/views.py
+++ doctor/views.py
@@ -10,14 +10,37 @@ from django.http import HttpResponse
 import plotly.figure_factory as ff
 from patient.models import Task
 from datetime import timedelta
+from datetime import datetime
+import math
+
+
+
 
 # def index(request):
 #     if request.user.is_authenticated and request.user.has_perm("doctor.doctor"):
 #         return render(request, "doctor.html", {})
 #     else:
 #         return redirect("/patient/")
-time = list(list() * 20)
 
+kind_dict = {i: index for index, i in enumerate(("抽血", "心電圖", "X光", "視力檢查", "聽力檢查"))}
+def get_base():
+    tasks = Task.objects.all()
+    time_stamp = math.inf
+    for task in tasks:
+        current = task.start_time.timestamp()
+        if task.start_time.timestamp() < time_stamp:
+            time_stamp = current
+    return datetime.fromtimestamp((current // 300)*300)
+
+def get_table():
+    base = get_base()
+    time_table = [list() for i in range(999)]
+    tasks = Task.objects.all()
+    for task in tasks:
+        now_time = task.start_time
+        now_index = int((now_time - base).total_seconds() // 300)
+        time_table[now_index].append(kind_dict[task.kind])
+    return time_table
 
 class HomeView(TemplateView):
     template_name = "doctor.html"
@@ -50,6 +73,7 @@ class HomeView(TemplateView):
         return render(request, self.template_name, {"form": form, "chart": string, "user": username})
 
     def post(self, request):
+        base = get_base()
         username = None
         if request.user.is_authenticated:
             username = request.user.username
@@ -57,14 +81,43 @@ class HomeView(TemplateView):
         if form.is_valid():
             instance = form.save(commit=False)
             kinds = json.loads(instance.kind.replace("'", '"'))
-            print(datetime.now())
+            print(kinds)
+            task_todo = list()
+            for i in kinds:
+                task_todo.append(kind_dict[i])
+
+            now_time = datetime.fromtimestamp(((int(datetime.now().timestamp()) + 300) // 300 + 1) * 300)
+            print(now_time, base)
+            now_index = int((now_time - base).total_seconds() // 300)
+            iterator = now_index  
+            print(iterator)
+            location = [None]*5
+            time = get_table()
+            print(time)
+            while True:
+                flag = 0
+                for i in task_todo:
+                    if i not in time[iterator]:
+                        print(time)
+                        time[iterator].append(i)
+                        location[i] = iterator
+                        iterator += 2
+                        flag = 1
+                        task_todo.remove(i)
+                        break
+                if len(task_todo) == 0:
+                    break
+                if flag != 1:
+                    iterator += 1
+        
+                        
+
             for index, kind in enumerate(kinds):
-                instance.kind = kind
                 Task.objects.create(
                     patient=instance.patient,
                     kind=kind,
                     doctor=username,
-                    start_time=timezone.now() + timedelta(minutes=5*index),
+                    start_time=base + timedelta(minutes=5*location[kind_dict[kind]]),
                 )
             user = User.objects.create_user(
                 username=instance.patient, password=instance.patient
